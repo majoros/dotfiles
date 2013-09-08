5eef6d072f24d22a6c18c0ac5717c1cb689c4b9f
Chris Majoros <chris@majoros.us>
2013-09-08 00:33:57 -0400

    CLeaned up misc.zsh

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -23,30 +23,3 @@ function epoch2date () {
 function calc () {
     echo "$*" | bc
 }
-
-function squirt () {
-    module use /ms/dist/iedinfra/modules
-    module load iedinfra/tools/prod
-    command squirt $*
-}
-
-function get_incr_tcms () {
-    module load ecm/tcm2cli
-    /ms/dist/ecm/PROJ/tcm2cli/qa/bin/tcm2 lr -type active -user lonstein -filter ReusableTcm=Yes -full 2>/dev/null| egrep -e 'Metaproject|Release|TcmId' |sort -u |sed 's/^\s*//'|while read ARG VAL
-    do
-        export $ARG=$VAL
-    done
-    export $(echo ${Metaproject}_INCR_TCM | tr "[:lower:]" "[:upper:]" )=$TcmId
-
-    /ms/dist/ecm/PROJ/tcm2cli/qa/bin/tcm2 lr -type active -user balusuv  -filter ReusableTcm=Yes -full 2>/dev/null| egrep -e 'Metaproject|Release|TcmId' |sort -u |sed 's/^\s*//'|while read ARG VAL
-    do
-        export $ARG=$VAL
-    done
-    export $(echo ${Metaproject}_INCR_TCM | tr "[:lower:]" "[:upper:]" )=$TcmId
-
-    unset TcmId
-    unset Metaproject
-    unset Release
-    unset _INCR_TCM
-    module unload ecm/tcm2cli
-}
